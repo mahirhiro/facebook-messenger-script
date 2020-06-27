6da45b43c1386940da57ba36d572ab81090b9a97
mahirhiro <mahirhiro@gmail.com>
2020-06-28 01:16:52 +0400

    Completed script.py.
    Time complexity: O(n)
    Space complexity: O(n)

--- script.py
+++ script.py
@@ -5,61 +5,100 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-USERNAME = 'mahirhiro'
-PASSWORD = 'password'
+USERNAME = 'test@gmail.com'
+PASSWORD = 'xyz'
 PATH = './drivers/chromedriver'
 URL = 'https://business.facebook.com/reducept/inbox/?business_id=641855079929'
 driver = webdriver.Chrome(PATH)
-
 driver.get(URL)
 
-# Get and enters the email address
-email = driver.find_element_by_id('email')
-email.send_keys(USERNAME)
 
-# Get and enters the password address
-password = driver.find_element_by_id('pass')
-password.send_keys(PASSWORD)
+def mark_message_as_done():
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.XPATH,
+             '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[3]/div/div[1]/div[1]/div/div/div[3]/div/div[6]'))
+    )
 
-# Gets and clicks the login button
-submit = driver.find_element_by_id('loginbutton')
-submit.click()
+    button_to_click_as_done = driver.find_element_by_xpath(
+        '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[3]/div/div[1]/div[1]/div/div/div[3]/div/div[6]')
+    button_to_click_as_done.click()
 
-# Navigates to the messenger button
-messenger_button = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[1]/div/div[1]/div[1]/a'))
-)
-messenger_button.click()
 
-# Waits until all the list of unread messages loads
-messenger_button = WebDriverWait(driver, 20).until(
-    EC.presence_of_all_elements_located(
-        (By.CLASS_NAME, '_5m17'))
-)
+def login():
+    # Get and enters the email address
+    email = driver.find_element_by_id('email')
+    email.send_keys(USERNAME)
 
-# Gets the users in a list
-list = driver.find_elements_by_class_name("_5m17")
+    # Get and enters the password address
+    password = driver.find_element_by_id('pass')
+    password.send_keys(PASSWORD)
+
+    # Gets and clicks the login button
+    submit = driver.find_element_by_id('loginbutton')
+    submit.click()
+
 
 # The for loop loops through the users and marks the conversation as done based on a certain message
-for x in range(0, len(list)):
-    try:
-        time.sleep(2)
-        if list[x].is_displayed():
-            list[x].click()
+def get_last_message():
+    # Gets the 'mark as done' button on facebook business to be able to move a converation to another folder
+    messages = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR,
+             '._3oh-._58nk'))
+    )
 
-            # Waits until the 'mark as done' button on facebook business appears
-            final = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[3]/div/div[1]/div[1]/div/div/div[3]/div/div[6]'))
-            )
-            # Gets the 'mark as done' button on facebook business to be able to move a converation to another folder
-            shit = mark_as_done = driver.find_element_by_xpath('//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[3]/div/div[1]/div[1]/div/div/div[3]/div/div[6]')
-            shit.click()
+    message = driver.find_elements_by_css_selector('._3oh-._58nk')
+    return message[len(message) - 1].text
 
-    except exceptions.StaleElementReferenceException as e:
-        # Gets the 'mark as done' button on facebook business to be able to move a converation to another folder
-        shit = mark_as_done = driver.find_element_by_xpath(
-            '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[3]/div/div[1]/div[1]/div/div/div[3]/div/div[6]')
-        shit.click()
-print('hi')
 
+def get_names_of_completed_conversations():
+    names = []
+
+    # Waits until all the list of unread messages loads
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, '_5m17'))
+    )
+
+    # Gets the users in a list
+    users_list = driver.find_elements_by_class_name("_5m17")
+
+    for x in range(0, len(users_list)):
+        if users_list[x].is_displayed():
+            users_list[x].click()
+
+            last_message = get_last_message()
+
+            if last_message == 'Bye bye for today (29/05/20)!!' or last_message == 'Bye bye for today (29/05/20)!':
+                names.append(driver.find_element_by_class_name('_iyo').text)
+    return names
+
+
+def click_on_messenger_section():
+    # Navigates to the messenger button
+    messenger_button = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="u_0_u"]/div/div/div/table/tbody/tr/td[1]/div/div[1]/div[1]/a'))
+    )
+    messenger_button.click()
+
+
+def main():
+    login()
+
+    click_on_messenger_section()
+
+    list_of_names = get_names_of_completed_conversations()
+
+    for name in list_of_names:
+        user = driver.find_element_by_xpath("//span[contains(text(),'" + name + "')]")
+        user.click()
+        try:
+            mark_message_as_done()
+        except exceptions.StaleElementReferenceException:
+            mark_message_as_done()
+
+
+if __name__ == "__main__":
+    main()
